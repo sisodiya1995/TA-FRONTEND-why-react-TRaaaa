9b1460e6f50a5a90759e9f4972f9b84241a507d1
Akash Singh <akashingh95@gmail.com>
2022-05-23 17:06:28 +0530

    articleApp

---
+++ block-BRaaao/code/myapp/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+import App from './components/app'
+import('./style/style.css')
+ReactDOM.render(<App/> ,document.getElementById('root'))
